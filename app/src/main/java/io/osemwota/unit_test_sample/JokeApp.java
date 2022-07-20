f316b944823267263fbcb4efbb4c05644659f635
David Osemwota <hirakoshinji722@gmail.com>
2022-07-20 16:48:10 +0100

    Add application class JokeApp

---
+++ app/src/main/java/io/osemwota/unit_test_sample/JokeApp.java
@@ -0,0 +1,11 @@
+package io.osemwota.unit_test_sample;
+
+import android.app.Application;
+
+public class JokeApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
